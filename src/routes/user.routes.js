bc0fea52b797c9ab7628a60dc1e7be6f6870f21b
JayashSaini <146090662+JayashSaini@users.noreply.github.com>
2024-02-01 18:34:29 +0530

    "Add Update, Get, Secure routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,9 +3,18 @@ import {
   registerUser,
   logoutUser,
   loginUser,
+  refreshAcessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJwt } from "../middlewares/auth.middlewares.js";
+
 const router = Router();
 
 router.route("/register").post(
@@ -21,6 +30,32 @@ router.route("/register").post(
   ]),
   registerUser
 );
-router.route("/logout").post(verifyJwt, logoutUser);
 router.route("/login").post(loginUser);
+
+// secured routes
+router.route("/logout").post(verifyJwt, logoutUser);
+router.route("/refresh-token").post(refreshAcessToken);
+router.route("change-password").post(verifyJwt, changeCurrentPassword);
+router.route("current-user").get(verifyJwt, getCurrentUser);
+router.route("update-account").patch(verifyJwt, updateAccountDetails);
+
+router
+  .route("update-avatar")
+  .patch(
+    verifyJwt,
+    upload.single("avatar"), // multer
+    updateUserAvatar
+  );
+
+router
+  .route("update-coverImage")
+  .patch(
+    verifyJwt,
+    upload.single("coverImage"), // multer
+    updateUserCoverImage
+  );
+
+router.route("/channel/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/history").get(verifyJwt, getWatchHistory);
+
 export default router;
