f4a25ea5dc151ee2885872a032438f13fbdcc5b3
JayashSaini <146090662+JayashSaini@users.noreply.github.com>
2024-02-02 18:06:28 +0530

    Remove console.log

--- src/utils/cloundinary.js
+++ src/utils/cloundinary.js
@@ -17,7 +17,6 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
 
-    console.log("File is uploded on cloudinary : ", response.url);
 
     fs.unlinkSync(localFilePath);
     return response;
