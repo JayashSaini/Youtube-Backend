cd239a6696ff05ae4b8c7493af13268ae990ca5e
JayashSaini <146090662+JayashSaini@users.noreply.github.com>
2024-02-03 19:27:58 +0530

    Import Commet, Like, Subscription, video, tweet, playlist Route file

--- src/app.js
+++ src/app.js
@@ -18,9 +18,24 @@ app.use(cookieParser());
 
 // import routes
 import userRouter from "./routes/user.routes.js";
+import likeRouter from "./routes/like.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import vidoeRouter from "./routes/vidoe.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+
 
 // routes decalaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/videos", vidoeRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+
 //http://localhost:8000/api/v1/users/register
 
 export { app };
