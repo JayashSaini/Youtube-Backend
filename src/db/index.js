84eef5d397e56dc24aa868b948414a2b9b09aeea
Jayash <jayashysaini7361@gmail.com>
2024-02-19 23:20:58 +0530

    "removes console.log"

--- src/db/index.js
+++ src/db/index.js
@@ -6,10 +6,6 @@ const app = express();
 async function connectDB() {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", () => {
-      console.log("ERROR", error);
-      throw error;
-    });
 
     console.log("MONGODB IS CONNECTED");
   } catch (error) {
